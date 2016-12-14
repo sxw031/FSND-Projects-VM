6f9e64943694ab8f78c31e047a4e94099202f98c
sxw031 <jameswangishere@gmail.com>
2016-12-14 17:31:06 +0800

    update webserver
    add the routing structures and create first template

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItems
@@ -15,12 +15,33 @@ session = DBSession()
 @app.route('/')
 @app.route('/catelog')
 def CategoryShow():
-    categories = session.query(Category).all()
-    output = ""
-    for category in categories:
-    	output += category.name
-    	output += "</br>"
-    return output
+    category = session.query(Category).all()
+    return render_template('main.html', category = category)
+
+
+@app.route('/catelog/<int:category_id>/new')
+def newCategory(category_id):
+	return "Page to create a new category. Task complete"
+
+@app.route('/catelog/<int:category_id>/edit')
+def editCategory(category_id):
+	return "Page to rename a category. Task complete"
+
+@app.route('/catelog/<int:category_id>/delete')
+def deleteCategory(category_id):
+	return "Page to delete a category. Task complete"
+
+@app.route('/catelog/<int:catelog_id>/<int:item_id>/new')
+def newCategoryItem(category_id, item_id):
+	return "Page to add a new item into the category. Task complete"
+
+@app.route('/catelog/<int:catelog_id>/<int:item_id>/edit')
+def editCategoryItem(category_id, item_id):
+	return "Page to edit an item into the category. Task complete"
+
+@app.route('/catelog/<int:category_id>/<int:item_id>/delete')
+def deleteCategoryItem(category_id, item_id):
+	return "Page to delete an item. Task complete"
 
 if __name__ == '__main__':
     app.debug = True
