ab354037fb29149ed2bfc0709f7c6af29a542701
sxw031 <jameswangishere@gmail.com>
2016-12-15 14:12:23 +0800

    add message Flashing

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItems
@@ -26,6 +26,7 @@ def newCategory():
 		newCat = Category(name = request.form['name'])
 		session.add(newCat)
 		session.commit()
+		flash("%s category is added!" % newCat.name)
 		return redirect(url_for('categoryShow'))
 	else:
 		return render_template('newcategory.html')
@@ -36,9 +37,11 @@ def editCategory(category_id):
 	editedCategory = session.query(Category).filter_by(id = category_id).one()
 	if request.method == 'POST':
 		if request.form['name']:
+			oldname = editedCategory.name
 			editedCategory.name = request.form['name']
 		session.add(editedCategory)
 		session.commit()
+		flash("%s is changed to %s!" % (oldname, editedCategory.name))
 		return redirect(url_for('categoryShow'))
 	else:
 		return render_template('editcategory.html', category_id = category_id, category = editedCategory)
@@ -50,6 +53,7 @@ def deleteCategory(category_id):
 	if request.method == 'POST':
 		session.delete(deletedCategory)
 		session.commit()
+		flash("%s category is deleted!" % deletedCategory.name)
 		return redirect(url_for('categoryShow'))
 	else:
 		return render_template('deletecategory.html', category_id = category_id, category = deletedCategory)
@@ -64,10 +68,12 @@ def itemPage(category_id):
 @app.route('/catelog/<int:category_id>/new', methods = ['GET','POST'])
 def newItem(category_id):
 	"""Page to add a new item to the specific category"""
+	category = session.query(Category).filter_by(id = category_id).one()
 	if request.method == 'POST':
 		newItem = CategoryItems(name = request.form['name'], description = request.form['description'],category_id=category_id)
 		session.add(newItem)
 		session.commit()
+		flash("'%s' is added to %s category!" % (newItem.name, category.name))
 		return redirect(url_for('itemPage', category_id=category_id))
 	else:
 		return render_template('newitem.html', category_id=category_id)
@@ -78,10 +84,12 @@ def editItem(category_id, item_id):
 	editedItem = session.query(CategoryItems).filter_by(id = item_id).one()
 	if request.method == 'POST':
 		if request.form['name']:
+			oldname = editedItem.name
 			editedItem.name = request.form['name']
 			editedItem.description = request.form['description']
 		session.add(editedItem)
 		session.commit()
+		flash("%s has been modified to %s!" % (oldname, editedItem.name))
 		return redirect(url_for('itemPage', category_id=category_id))
 	else:
 		return render_template('edititem.html', category_id = category_id, item_id = item_id, item = editedItem)
@@ -89,15 +97,18 @@ def editItem(category_id, item_id):
 @app.route('/catelog/<int:category_id>/<int:item_id>/delete', methods=['GET','POST'])
 def deleteItem(category_id, item_id):
 	"""Page to delete a item."""
+	category = session.query(Category).filter_by(id=category_id).one()
 	deletedItem = session.query(CategoryItems).filter_by(id = item_id).one()
 	if request.method == 'POST':
 		session.delete(deletedItem)
 		session.commit()
+		flash("%s is deleted from category %s!" % (deletedItem.name, category.name))
 		return redirect(url_for('itemPage', category_id=category_id))
 	else:
 		return render_template('deleteitem.html', category_id = category_id, item_id = item_id, item = deletedItem)
 	
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8888)
+	app.secret_key = 'super_secret_key'
+	app.debug = True
+	app.run(host='0.0.0.0', port=8888)
