522868b3563d16b142d874479bc576bef9ef3053
sxw031 <jameswangishere@gmail.com>
2016-12-21 10:34:28 +0800

    fix the facebook login problem
    Turn off the 'Require App Secret' option in the facebook app setting

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -137,50 +137,51 @@ def gconnect():
 @app.route('/catalog/fbconnect', methods=['POST'])
 def fbconnect():
 	"""Managees the Facebook authentication process for login."""
+
 	if request.args.get('state') != login_session['state']:
 		response = make_response(json.dumps('Invalid state parameter.'), 401)
 		response.headers['Content-Type'] = 'application/json'
 		return response
-	access_token = request.data
 
-	app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']['app_id']
-	app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-	url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
+	# json.loads will laod a json object into a python dict, json.dumps will dump a python dict to a json object
+	data = json.loads(request.data)
+	access_token = data['access_token']
+	# print "access token received %s " % access_token
+	
+	app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+	app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+	url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
 	h = httplib2.Http()
 	result = h.request(url, 'GET')[1]
 
 	# Use token to get user info from API
-	userinfo_url = "https://graph.facebook.com/v2.4/me"
+	userinfo_url = "https://graph.facebook.com/v2.8/me"
 	# strip expire tag from access token
 	token = result.split("&")[0]
 
-	url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+
+	url = 'https://graph.facebook.com/v2.8/me?%s&fields=name,id,email' % token
 	h = httplib2.Http()
 	result = h.request(url, 'GET')[1]
 	# print "url sent for API access:%s"% url
 	# print "API JSON result: %s" % result
 	data = json.loads(result)
-
 	login_session['provider'] = 'facebook'
-	login_session['username'] = data["name"]
-	login_session['email'] = data["email"]
-	login_session['facebook_id'] = data["id"]
+	login_session['username'] = data['name']
+	login_session['email'] = data['email']
+	login_session['facebook_id'] = data['id']
 
-	# The token must be stored in the login_session in order to properly logout
-	# let's strip out the information before the equals sign in our token
+	# The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
 	stored_token = token.split("=")[1]
 	login_session['access_token'] = stored_token
 
 	# Get user picture
-	url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+	url = 'https://graph.facebook.com/v2.8/me/picture?%s&redirect=0&height=200&width=200' % token
 	h = httplib2.Http()
 	result = h.request(url, 'GET')[1]
 	data = json.loads(result)
 
-	login_session['picture'] = data["data"]["url"]
+	login_session['picture'] = data['data']['url']
 
 	# see if user exists
 	user_id = getUserID(login_session['email'])
@@ -195,8 +196,7 @@ def fbconnect():
 	output += '!</h1>'
 	output += '<img src="'
 	output += login_session['picture']
-	output += '" style = "width: 300px; height: 300px;border-radius: 150px;'
-	output += '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+	output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
 
 	flash("Now logged in as %s" % login_session['username'])
 	return output
